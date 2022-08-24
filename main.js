ca8a1be51907455cdc7f01ccdf69c1b486ca0225
Jun3-dev <jun.lee73@outlook.com>
2022-08-24 21:34:09 +1000

    i cannot write commit messages

--- main.js
+++ main.js
@@ -28,3 +28,25 @@ app.on('window-all-closed', () => {
     app.quit()
   }
 })
+
+
+
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
